98098f139104ed1f7796a1a8ee236c9ac22e270e
0307aaron <aaron@AarondeMacBook-Pro.local>
2023-02-23 10:47:33 -0800

    A3

--- chart1.R
+++ chart1.R
@@ -1,5 +1,7 @@
 dataset <- read.csv("A3 Dataset.csv", stringsAsFactors = FALSE)
-C1 <- dataset %>% group_by(MaterialType) %>% count()
+
+C1 <- dataset %>% filter(MaterialType == "EBOOK" | MaterialType == "MOVIE" | MaterialType == "MAGAZINE")  %>% group_by(MaterialType) %>% count()
+
 plot <- ggplot(C1, aes(x="", y=n, fill=MaterialType)) +
   geom_bar(width=1, stat="identity") +
   coord_polar("y", start=0) +
